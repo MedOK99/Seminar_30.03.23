a6e3207f4b362b94a4378256a5dec93ca68737f8
MedOK99 <127406946+MedOK99@users.noreply.github.com>
2023-03-30 22:06:04 +0300

    Решил задачу ч кубом

--- Task004/Program.cs
+++ Task004/Program.cs
@@ -3,9 +3,22 @@
 от 1 до N в третьей степени.
 
 double number = 3;
+Console.WriteLine(Math.Pow(number, 3));  
+_____________________________________
+for (int i = 0; i < 10; i += 2)
+{
+    Console.Write(i + ", ");
+}
 
-Console.WriteLine(Math.Pow(number, 3));  */
+Console.WriteLine();
 
+int counter = 0;
+while(counter < 10)
+{
+    Console.Write(counter + ", ");
+    counter = counter + 2;
+}
+--------------------------------------- */
 
 Console.Clear();
 
