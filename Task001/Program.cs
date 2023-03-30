400120395a5cc45c162eba6e08ca34b68f581846
MedOK99 <127406946+MedOK99@users.noreply.github.com>
2023-03-30 20:32:33 +0300

    Task001

---
+++ Task001/Program.cs
@@ -0,0 +1,28 @@
+﻿// 1 : x > 0 И y > 0
+// Напишите программу, которая по заданному номеру четверти, 
+// показывает диапазон возможных координат точек в этой 
+// четверти (x и y).
+
+Console.WriteLine("Введите четверть");
+int x = int.Parse(Console.ReadLine());
+
+if (x == 1)
+{
+  Console.WriteLine("x > 0 и y > 0");
+}
+else if (x == 2)
+{
+  Console.WriteLine("x < 0 и y > 0");
+}
+else if (x == 3)
+{
+  Console.WriteLine("x < 0 и y < 0");
+}
+else if (x == 4)
+{
+  Console.WriteLine("x > 0 и y < 0");
+}
+else
+{
+  Console.WriteLine("Не верное значение четверти!");
+}
